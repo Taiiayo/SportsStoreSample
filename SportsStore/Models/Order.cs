40d7a4c84426d2f2fa23af37595dffda0ab30298
vsh <vsh@adaperio.ru>
2019-05-08 12:48:31 +0300

    first commit

---
+++ SportsStore/Models/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace SportsStore.Models
+{
+    public class Order
+    {
+        [BindNever]
+        public int OrderID { get; set; }
+        [BindNever]
+        public ICollection<Cart.CartLine> Lines { get; set; }
+        [Required(ErrorMessage = "Please enter a name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter the first address line")]
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Line3 { get; set; }
+
+        [Required(ErrorMessage = "Please enter city name")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Enter state name")]
+        public string State { get; set; }
+
+        public string Zip { get; set; }
+
+        [Required(ErrorMessage = "Enter a country name")]
+        public string Country { get; set; }
+        public bool GiftWrap { get; set; }
+    }
+}
